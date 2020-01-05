ab2eefd0ba4b8ea7611af3cb47c88700acdae401
Lee19whut <377200300@qq.com>
2020-01-05 15:47:37 +0900

    异常捕获

--- Chap1/C1_23.py
+++ Chap1/C1_23.py
@@ -1,8 +1,11 @@
 def error(a):
 
-    for i in range(6):
-        if i > len(a):
-            raise IndexError('Do not try overflow attacks in Python')
-    return  None
+
+    try:
+        b = a[5]
+    except IndexError:
+        print('''Don't try buffer overflow attacks in Python!''')
+
+
 
 error([1,2,3])
