9099b27bac5e12a03936f7436090b812d5a2271a
Lee19whut <377200300@qq.com>
2020-01-12 17:46:33 +0900

    装饰器

---
+++ Chap2/decorator.py
@@ -0,0 +1,15 @@
+def check(func):
+    def inside(a, b):
+        if b == 0:
+            print('不能除0')
+            return 
+        func(a, b)
+    return inside
+
+@check
+def div(a, b):
+    return a / b
+
+#div = check(div)
+
+print(div(4, 0))
