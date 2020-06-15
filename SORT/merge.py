afc33c5f7973e30792a8bbf66c0da1bdd32799cf
Lee19whut <377200300@qq.com>
2020-06-15 14:44:53 +0900

    归并排序nlgn

--- SORT/merge.py
+++ SORT/merge.py
@@ -13,7 +13,7 @@ def merge(s1, s2):
             k += 1
 
             if i == len(s1):
-                s[k] = s2[j]
+                s[k:] = s2[j:]
 
         else:
             s[k] = s2[j]
@@ -21,12 +21,25 @@ def merge(s1, s2):
             k += 1
 
             if j == len(s2):
-                s[k] = s1[i]
+                s[k:] = s1[i:]
 
         
 
     return s
 
 
-print(merge([1,1,3],[1,2,4]))
+def sort_merge(s):
+    n = len(s)
+    if n < 2:
+        return s
+    mid = n // 2
+    s1 = s[0:mid]
+    s2 = s[mid:n]
+
+    sort_merge(s1)
+    sort_merge(s2)
+    merge(s1,s2)
+
+
+print(merge([1,3],[2,4,5,6,7]))
 
